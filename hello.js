4fd56e23602cd394244bdeae941c2a6f3172abe3
rosohakateryna <58200664+rosohakateryna@users.noreply.github.com>
2020-07-29 17:52:14 +0300

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,17 @@
+
+function myFunction(){
+    var age=document.getElementById('id1').value
+    if  (age >= 21 && age <= 44)
+      alert("Welcome");
+    if  (age >= 1 && age <= 17)
+      alert("Визиваємо поліцію");
+    if  (age >= 18 && age <= 21)
+      alert("Недостатній вік");
+    if  (age >= 45 && age <= 99)
+      alert("Ви застаріли");
+   
+    
+}
+
+    
+
